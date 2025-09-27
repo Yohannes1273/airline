fd8a75aef04d2a06ed078364ce69eb414e156d1e
Yohannes1273 <yohannesgebeyehu73@gmail.com>
2025-09-27 18:43:47 +0300

    model

--- flights/models.py
+++ flights/models.py
@@ -5,4 +5,5 @@ class Flight(models.Model):
     orgin=models.CharField(max_length=64)
     destination=models.CharField(max_length=64)
     duration=models.IntegerField()
-    
+    def __str__(self):
+        return f"{self.origin} to {self.destination}"
--- flights/urls.py
+++ flights/urls.py
@@ -2,3 +2,8 @@
 
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    
+]
+
