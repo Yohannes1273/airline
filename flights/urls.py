fac44ccc9d37a5689e5c2e4355199273ae5e5c19
Yohannes1273 <yohannesgebeyehu73@gmail.com>
2025-09-28 16:08:23 +0300

    updated

--- flights/urls.py
+++ flights/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name="index")
+    path("",views.index, name="index"),
+    path("<int:flight_id>",views.flight,name="flight")
     
 ]
 
